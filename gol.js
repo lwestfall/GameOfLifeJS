1dd107b56e97dccdce187b1891a7933f72570e42
Luke Westfall <westfalll@harringtonhoists.com>
2022-12-20 08:05:17 -0500

    add onclick handler to cells and track state

--- gol.js
+++ gol.js
@@ -1,11 +1,15 @@
-var rowCount = 20;
-var colCount = 20;
+var rowCount = 50;
+var colCount = 50;
 
 var cells = [];
+var aliveBuffer = [];
+
 addEventListener("load", (ev) => {
     var tableBody = document.getElementById('gol-table-body');
 
     for (var i = 0; i < rowCount; i++) {
+        aliveBuffer.push(new Array(colCount).fill(0));
+
         var cellArr = [];
         cells.push(cellArr);
 
@@ -14,6 +18,20 @@ addEventListener("load", (ev) => {
         for (var j = 0; j < colCount; j++) {
             var cell = row.insertCell(j);
             cellArr.push(cell);
+            cell.onclick = (event) => handleCellClick(event);
         }
     }
 });
+
+function handleCellClick(event) {
+    var cell = event.srcElement;
+    var row = cell.parentElement;
+
+    var i = row.rowIndex;
+    var j = cell.cellIndex;
+
+    var wasAlive = aliveBuffer[i][j];
+    aliveBuffer[i][j] = wasAlive ? 0 : 1;
+    console.log(wasAlive);
+    cell.style.backgroundColor = wasAlive ? 'white': 'black';
+}
