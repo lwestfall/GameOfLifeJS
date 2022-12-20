864265da4eed5e4da6b48161ac696c6baa386d59
Luke Westfall <westfalll@harringtonhoists.com>
2022-12-20 12:32:04 -0500

    add game logic and hook up controls

--- gol.js
+++ gol.js
@@ -1,11 +1,31 @@
-var rowCount = 50;
-var colCount = 50;
+var rowCount = 0;
+var colCount = 0
+var timeStepSeconds = 1;
 
-var cells = [];
-var aliveBuffer = [];
+var cells;
+var aliveBuffer;
+
+var tableBody;
+var rowCountInput;
+var colCountInput;
+var timeStepSecondsInput;
+
+var intervalId;
 
 addEventListener("load", (ev) => {
-    var tableBody = document.getElementById('gol-table-body');
+    tableBody = document.getElementById('gol-table-body');
+    rowCountInput = document.getElementById('row-count');
+    colCountInput = document.getElementById('col-count');
+    timeStepSecondsInput = document.getElementById('time-step');
+    handleSettingsChange();
+    initTable();
+});
+
+function initTable()
+{
+    tableBody.innerHTML = '';
+    cells = [];
+    aliveBuffer = [];
 
     for (var i = 0; i < rowCount; i++) {
         aliveBuffer.push(new Array(colCount).fill(0));
@@ -21,7 +41,7 @@ addEventListener("load", (ev) => {
             cell.onclick = (event) => handleCellClick(event);
         }
     }
-});
+}
 
 function handleCellClick(event) {
     var cell = event.srcElement;
@@ -32,6 +52,89 @@ function handleCellClick(event) {
 
     var wasAlive = aliveBuffer[i][j];
     aliveBuffer[i][j] = wasAlive ? 0 : 1;
-    console.log(wasAlive);
     cell.style.backgroundColor = wasAlive ? 'white': 'black';
+}
+
+function handleSettingsChange() {
+    rowCount = rowCountInput.value;
+    colCount = colCountInput.value;
+    timeStepSeconds = timeStepSecondsInput.value;
+
+    var wasPlaying = intervalId != null;
+
+    if (wasPlaying)
+    {
+        handlePauseClick();
+        handlePlayClick();
+    }
+}
+
+function tick() {
+    var nextState = [];
+
+    for (var i = 0; i < rowCount; i++) {
+        var row = tableBody.rows.item(i);
+        nextState.push(new Array(colCount).fill(0));
+
+        for (var j = 0; j < colCount; j++) {
+            nextState[i][j] = getNextState(i, j);
+            var cell = row.cells.item(j);
+            cell.style.backgroundColor = nextState[i][j] ? 'black': 'white';
+        }
+    }
+
+    aliveBuffer = nextState;
+}
+
+function getNextState(rowIndex, colIndex) {
+    var currentlyAlive = aliveBuffer[rowIndex][colIndex];
+    var livingNeighbors = 0;
+
+    for (var i = rowIndex - 1; i <= rowIndex + 1; i++)
+    {
+        if (i < 0 || i > rowCount - 1) continue;
+
+        for (var j = colIndex - 1; j <= colIndex + 1; j++)
+        {
+            if (j < 0 || j > colCount - 1 || (i == rowIndex && j == colIndex)) continue;
+
+            if (aliveBuffer[i][j]) {
+                livingNeighbors++;
+                if (livingNeighbors > 3) return 0;
+            }
+        }
+    }
+
+    if (livingNeighbors < 2 || livingNeighbors > 3) {
+        return 0;
+    }
+
+    if (livingNeighbors == 3 || (livingNeighbors == 2 && currentlyAlive))
+    {
+        return 1;
+    }
+
+    return 0;
+}
+
+function handlePlayClick() {
+    if (intervalId == null) {
+        tick();
+        intervalId = setInterval(() => tick(), timeStepSeconds * 1000);
+    }
+}
+
+function handlePauseClick() {
+    if (intervalId != null)
+    {
+        clearInterval(intervalId);
+        intervalId = null;
+    }
+}
+
+function handleStepClick() {
+    if (intervalId == null)
+    {
+        tick();
+    }
 }
